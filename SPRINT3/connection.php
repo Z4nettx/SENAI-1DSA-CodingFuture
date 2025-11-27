e7c521ba8c9e64ae10fdc5a0b032cfd348c37e1b
Eduardo Zanetti Luis <134246431+Z4nettx@users.noreply.github.com>
2025-11-27 16:28:23 -0300

    Add files via upload

---
+++ SPRINT3/connection.php
@@ -0,0 +1,11 @@
+<?php 
+
+$servername = 'localhost';
+
+$username = 'root';
+
+$password = 'Senai@118';
+
+$dbname = 'cervejariadogma';
+
+$conn = new mysqli($servername, $username, $password, $dbname);
