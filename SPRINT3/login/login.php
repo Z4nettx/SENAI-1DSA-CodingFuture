3f2fa154f738f799d940cb803dd59a2e55d87ad6
Eduardo Zanetti Luis <134246431+Z4nettx@users.noreply.github.com>
2025-11-28 16:38:09 -0300

    Add files via upload

---
+++ SPRINT3/login/login.php
@@ -0,0 +1,27 @@
+<?php
+include '../connection.php';
+
+$mensagem = ''; // variável para exibir no HTML
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $email = $_POST['email'] ?? '';
+    $senha = $_POST['senha'] ?? '';
+    
+    if ($email == "" || $senha == "") {
+        $mensagem = "Campos nulos. Tente novamente";
+    } else {
+    // Verifica se o usuário existe
+    $sql = "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha'";
+    $resultado = $conn->query($sql);
+
+    if ($resultado && $resultado->num_rows > 0) {
+        // Login válido → redireciona
+        header("Location: ../paineladmin/index.php");
+        exit;
+    } else {
+        header("Location: erro.php");
+    }
+    }
+}
+
---
+++ SPRINT3/readme.md
@@ -0,0 +1,15 @@
+# SPRINT 3
+
+## Na SPRINT 3, a equipe se dedicou a conectar o banco de dados ao sistema HTML e CSS por meio do PHP. ⚙️
+
+#### Os desenvolvedores fizeram os códigos PHP para receber os dados, tratá-los, executar o login e o cadastro de produtos no banco de dados.
+
+### A empresa utilizou as seguintes ferramentas e tecnologias: 🖥️
+
+<ul>
+    <em>
+        <li>Visual Studio Code</li>
+        <li>Apache</li>
+    </em>
+
+</ul>
