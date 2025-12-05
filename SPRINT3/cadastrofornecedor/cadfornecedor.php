00f96d0b3be182b7918cfbc37750d92c5e62a080
Eduardo Zanetti Luis <134246431+Z4nettx@users.noreply.github.com>
2025-12-05 10:45:53 -0300

    Add files via upload

---
+++ SPRINT3/cadastrofornecedor/cadfornecedor.php
@@ -0,0 +1,22 @@
+<?php
+include '../connection.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'] ?? '';
+    $pais = $_POST['pais'] ?? '';
+    $endereco = $_POST['endereço'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $cnpj = $_POST['cnpj'] ?? '';
+    $observacoes = $_POST['observacoes'] ?? '';
+    $estado = $_POST['estado'] ?? '';
+
+    $sql = "INSERT INTO cadastrofornecedores (nome, pais, endereço, email, cnpj, observacoes, estado) VALUES ('$nome','$pais','$endereco', '$email', '$cnpj','$observacoes','$estado')";
+
+    if ($conn->query($sql) === TRUE) {
+        header('Location: cadastrado.php');
+    } else {
+        echo "Erro: " . $conn->error;
+    }   
+}
+
+// $conn->close();
